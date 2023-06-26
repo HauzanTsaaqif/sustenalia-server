92dcb559af6ad84a282e46f6981b57e7ff935a90
Hauzan Tsaaqif Mushaddaq <hauzantsaaqif28@gmail.com>
2023-06-26 08:09:17 +0700

    new

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const PDFDocument = require('pdfkit');
+const pdfFonts = require('pdfmake/build/vfs_fonts');
+
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: 'postgres://default:LO1wdI2GgHWV@ep-tiny-limit-795119.us-east-1.postgres.vercel-storage.com:5432/verceldb',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/data-account', (req, res) => {
+  const sql = 'SELECT * FROM data_account';
+  
+  pool.query(sql, (err, result) => {
+    if (err) {
+      console.error('Gagal menjalankan query:', err);
+      return res.json(err);
+    } else {
+      return res.json(result.rows);
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+    return res.json("succesfull");
+})
+
+app.listen(3306, () =>{
+    console.log("listening");
+})
